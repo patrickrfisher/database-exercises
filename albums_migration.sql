fc3603bf310621eb6b1cf87e47cb8b8a99867884
patrickrfisher <patrick.ryan.fisher@gmail.com>
2018-10-03 14:24:53 -0500

    basic statements check

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-artist VARCHAR(50),
-artist_name VARCHAR(50) NOT NULL,
+artist VARCHAR(255),
+artist_name VARCHAR(255) NOT NULL,
 release_date INT NOT NULL,
 sales FLOAT NOT NULL,
-genre VARCHAR(50),
+genre VARCHAR(255),
 PRIMARY KEY (id)
 );
